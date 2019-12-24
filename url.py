f9e30dc0013bbd700d71582b54825c3e56ec6535
Christian Bern <verylonely@protonmail.ch>
2019-12-24 08:41:05 +0300

    change token to token example

--- url.py
+++ url.py
@@ -1,7 +1,6 @@
-TOKEN = '749909791:AAETqVK1V_uv17x0IIAcYN_uVkpoxIm2EEg/'
+TOKEN = '000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/'
 chat_id = '&chat_id='
 text = '&text='
 textplus = '[BOT] '
 sendMessage = 'https://api.telegram.org/bot' + TOKEN + 'sendMessage?'
 hello = 'Я могу свести тебя со случайным человеком\nЧтобы начать поиск введите /run'
-
